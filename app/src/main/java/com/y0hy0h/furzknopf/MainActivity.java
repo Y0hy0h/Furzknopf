b8bfe3b5504740578d25f11a0c735f53b8afad5e
Y0hy0h <Y0hy0h@users.noreply.github.com>
2015-08-26 01:46:57 +0200

    Clean up code, update to API level 23

--- app/src/main/java/com/y0hy0h/furzknopf/MainActivity.java
+++ app/src/main/java/com/y0hy0h/furzknopf/MainActivity.java
@@ -6,12 +6,11 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 
 import java.io.IOException;
