c678c13572efc758513a7326ebd4c2e1c438ad69
Y0hy0h <y0hy0h@users.noreply.github.com>
2015-08-27 22:12:12 +0200

    Add basic widget files

---
+++ app/src/main/java/com/y0hy0h/furzknopf/FartbuttonWidget.java
@@ -0,0 +1,25 @@
+package com.y0hy0h.furzknopf;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class FartbuttonWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
