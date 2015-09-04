85526bd583311c99ff23d3f1c02c7bb4eac58d50
Y0hy0h <y0hy0h@users.noreply.github.com>
2015-09-04 14:11:21 +0200

    Add basic AudioTrack playback, make audio assets mono PCM

--- app/src/main/java/com/y0hy0h/furzknopf/widget/VolatileFartService.java
+++ app/src/main/java/com/y0hy0h/furzknopf/widget/VolatileFartService.java
@@ -3,15 +3,15 @@ package com.y0hy0h.furzknopf.widget;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.media.MediaPlayer;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.y0hy0h.furzknopf.Utility;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 
 public class VolatileFartService extends Service {
@@ -22,38 +22,37 @@ public class VolatileFartService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_PLAY_FART.equals(action)) {
                 Log.v(LOG_TAG, "Intent received");
 
+                // Get the file to be played.
                 String pathToFile = String.format(
                         Locale.US,
-                        "fart%02d.ogg",
-                        Utility.getIntBetween(1, 15)
+                        "fart%02d.wav",
+                        4
                 );
 
+                // Load file and play.
+                InputStream audioIS = null;
+
                 try {
-                    AssetFileDescriptor assetFD = getAssets().openFd(pathToFile);
-                    MediaPlayer tempMediaPlayer = new MediaPlayer();
-                    tempMediaPlayer.setDataSource(
-                            assetFD.getFileDescriptor(),
-                            assetFD.getStartOffset(),
-                            assetFD.getLength()
-                    );
-                    assetFD.close();
-                    tempMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        @Override
-                        public void onCompletion(MediaPlayer mp) {
-                            mp.release();
-                            stopSelf();
-                        }
-                    });
-                    tempMediaPlayer.prepare();
-                    Log.v(LOG_TAG, "Start playing");
-                    tempMediaPlayer.start();
+                    audioIS = getAssets().openFd(pathToFile).createInputStream();
+                    playSound(audioIS);
+
                 } catch (IOException e) {
                     Log.e(LOG_TAG, "File "+pathToFile+" could not be loaded.", e);
+                } finally {
+                    // Close InputStream.
+                    if (audioIS != null) {
+                        try {
+                            audioIS.close();
+                        } catch (IOException e) {
+                            Log.e(LOG_TAG, "Error closing InputStream", e);
+                        }
+                    }
                 }
             }
         }
@@ -61,11 +60,61 @@ public class VolatileFartService extends Service {
         return START_NOT_STICKY;
     }
 
+    /**
+     * Plays the sound that is represented by the given InputStream.
+     * Uses AudioTrack and only works on mono 16 bit PCM .wav files.
+     * @param audioIS The InputStream of the sound to be played.
+     * @throws IOException
+     */
+    private void playSound(InputStream audioIS) throws IOException {
+
+        // Get .wav header info and generate appropriate AudioTrack.
+        WavInfo info = WavDecoder.readHeader(audioIS);
+        AudioTrack at = new AudioTrack(
+                AudioManager.STREAM_MUSIC,
+                info.mRate,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                info.mDataSize,
+                AudioTrack.MODE_STATIC
+        );
+
+        // Write the InputStream's data into the AudioTrack.
+        at.write(WavDecoder.readWavPcm(info, audioIS), 0, info.mDataSize);
+
+        // Set up listener for sound completely playing.
+        at.setNotificationMarkerPosition(info.mDataSize / 4);
+        at.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener() {
+            @Override
+            public void onMarkerReached(AudioTrack track) {
+                // Release the resources after playing.
+                Log.v(LOG_TAG, "Finished playing");
+                track.release();
+
+                // Sound has been played, stop service.
+                stopSelf();
+            }
+
+            @Override
+            public void onPeriodicNotification(AudioTrack track) {
+            }
+        });
+
+        // Start playback.
+        at.play();
+    }
+
     public static Intent createIntentPlayFart(Context context) {
         Intent intent = new Intent(context, VolatileFartService.class);
         intent.setAction(ACTION_PLAY_FART);
 
         return intent;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(LOG_TAG, "Service being destroyed");
     }
 
     @Nullable
---
+++ app/src/main/java/com/y0hy0h/furzknopf/widget/WavDecoder.java
@@ -0,0 +1,76 @@
+package com.y0hy0h.furzknopf.widget;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ * Code partly taken from
+ * http://mindtherobot.com/blog/580/android-audio-play-a-wav-file-on-an-audiotrack/.
+ */
+public class WavDecoder {
+
+    private static final String LOG_TAG = WavDecoder.class.getSimpleName();
+
+    private static final String RIFF_HEADER = "RIFF";
+    private static final String WAVE_HEADER = "WAVE";
+    private static final String FMT_HEADER = "fmt ";
+    private static final String DATA_HEADER = "data";
+
+    private static final int HEADER_SIZE = 44;
+
+    private static final String CHARSET = "ASCII";
+
+    public static WavInfo readHeader(InputStream wavStream)
+            throws IOException {
+
+        ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        int read = wavStream.read(buffer.array(), 0, HEADER_SIZE);
+        if (read != HEADER_SIZE) {
+            Log.e(LOG_TAG, "Error reading header of wav file");
+        }
+        buffer.rewind();
+
+        buffer.position(20);
+        int format = buffer.getShort();
+        checkFormat(format == 1, "Unsupported encoding: " + format); // 1 means Linear PCM
+        int channels = buffer.getShort();
+        checkFormat(channels == 1, "Unsupported channels: " + channels);
+        int rate = buffer.getInt();
+        checkFormat(rate <= 48000 && rate >= 11025, "Unsupported rate: " + rate);
+
+        buffer.position(34);
+        int bits = buffer.getShort();
+        checkFormat(bits == 16, "Unsupported bits: " + bits);
+
+        buffer.position(40);
+        int dataSize = buffer.getInt();
+        checkFormat(dataSize > 0, "wrong datasize: " + dataSize);
+
+        return new WavInfo(channels, rate, bits, dataSize);
+    }
+
+    private static void checkFormat(boolean valid, String errorMessage) throws IOException{
+        if (!valid) {
+            IOException e = new IOException();
+            Log.e(LOG_TAG, errorMessage, e);
+            throw e;
+        }
+
+    }
+
+    public static byte[] readWavPcm(WavInfo info, InputStream stream) throws IOException {
+        byte[] data = new byte[info.mDataSize];
+        int read = stream.read(data, 0, data.length);
+        if (read != data.length) {
+            Log.e(LOG_TAG, "Error reading wav file");
+        }
+
+        return data;
+    }
+}
