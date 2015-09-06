95d36b8e5777b984645262b37dc8a58cb052c61f
Y0hy0h <y0hy0h@users.noreply.github.com>
2015-09-06 16:24:32 +0200

    Document WavDecoder

--- app/src/main/java/com/y0hy0h/furzknopf/widget/WavDecoder.java
+++ app/src/main/java/com/y0hy0h/furzknopf/widget/WavDecoder.java
@@ -15,27 +15,31 @@ public class WavDecoder {
 
     private static final String LOG_TAG = WavDecoder.class.getSimpleName();
 
-    private static final String RIFF_HEADER = "RIFF";
-    private static final String WAVE_HEADER = "WAVE";
-    private static final String FMT_HEADER = "fmt ";
-    private static final String DATA_HEADER = "data";
-
     private static final int HEADER_SIZE = 44;
 
-    private static final String CHARSET = "ASCII";
-
+    /**
+     * Returns a WavInfo object containing the header information
+     * of the .wav file provided as an InputStream.
+     * @param wavStream The InputStream of the .wav file to have the header information extracted.
+     * @return A WavInfo object containing the header information of the InputStream,
+     * if it is usable by the app.
+     * @throws IOException
+     */
     public static WavInfo readHeader(InputStream wavStream)
             throws IOException {
 
+        // Initialize buffer.
         ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
 
+        // Load header into buffer.
         int read = wavStream.read(buffer.array(), 0, HEADER_SIZE);
         if (read != HEADER_SIZE) {
             Log.e(LOG_TAG, "Error reading header of wav file");
         }
         buffer.rewind();
 
+        // Read format, channels, rate, bits and datsize.
         buffer.position(20);
         int format = buffer.getShort();
         checkFormat(format == 1, "Unsupported encoding: " + format); // 1 means Linear PCM
@@ -55,6 +59,12 @@ public class WavDecoder {
         return new WavInfo(channels, rate, bits, dataSize);
     }
 
+    /**
+     * Logs the provided error message if not valid.
+     * @param valid The condition to be checked.
+     * @param errorMessage The error message to be printed.
+     * @throws IOException
+     */
     private static void checkFormat(boolean valid, String errorMessage) throws IOException{
         if (!valid) {
             IOException e = new IOException();
@@ -64,6 +74,14 @@ public class WavDecoder {
 
     }
 
+    /**
+     * Reads the PCM data from the provided InputStream
+     * using information from the provided WavInfo.
+     * @param info The header information on the provided InputStream.
+     * @param stream The PCM data to load.
+     * @return A byte array containing the raw PCM data.
+     * @throws IOException
+     */
     public static byte[] readWavPcm(WavInfo info, InputStream stream) throws IOException {
         byte[] data = new byte[info.mDataSize];
         int read = stream.read(data, 0, data.length);
